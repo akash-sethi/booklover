5aa650021b80fcb794220e6bd981970bb6bb8c64
sethi,akash <akash.sethi@aegon.nl>
2019-01-29 22:49:06 +0100

    api for fetch data

--- src/api.js
+++ src/api.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import app from "./base";
 
 export default {
@@ -10,5 +11,10 @@ export default {
       app
         .auth()
         .createUserWithEmailAndPassword(credentials.email, credentials.password)
+  },
+  books: {
+    fetchAll: () => axios.get("/api/books").then(res => res.data.books),
+    create: book =>
+        axios.post("/api/books", { book }).then(res => res.data.book)
   }
 };
